7085f06eb3183ed1ae089bc9ae189dd6af35f7c5
AnnaKalis <annkalis@gmail.com>
2024-02-27 14:38:43 +0100

    navigation desktop and mobile

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const hamburger =document.querySelector(".hamburger");
+const navMenu =document.querySelector(".nav-menu");
+
+function onToggleClick(){
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+}
+
+export function toggleMobileNav() {
+    hamburger.addEventListener("click", onToggleClick)
+}
+
+toggleMobileNav();
